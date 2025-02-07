428141d102023784c3479688d7d3cd0165b6a534
Nisarg Patel <23csd11@svitvasad.ac.in>
2025-02-07 23:20:21 +0530

    minor changes again

--- src/Components/Star.jsx
+++ src/Components/Star.jsx
@@ -1,26 +1,34 @@
 import { useEffect } from "react";
-import "./style.css" // Make sure to include appropriate styling
+import "./style.css"; // Make sure to include appropriate styling
 import { Rive } from "@rive-app/canvas";
 
-
 export default function Star() {
-    useEffect(() => {
-       const s = new Rive({
-        src:'/onlystars.riv',
-        canvas:document.getElementById("starComponent"),
-        autoplay:true,
-        stateMachines:"State Machine 1",
-        onLoad: function () {
-            s.resizeDrawingSurfaceToCanvas();
-        },
-       })
-    }, []);
+  useEffect(() => {
+    const s = new Rive({
+      src: '/onlystars.riv',
+      canvas: document.getElementById("starComponent"),
+      autoplay: true,
+      stateMachines: "State Machine 1",
+      onLoad: function () {
+        s.resizeDrawingSurfaceToCanvas();
+      },
+    });
 
-    return (
-        <>
-        <div className="star">
-            <canvas id="starComponent"></canvas>
-        </div>
-        </>
-    );
+    // Resize the canvas on window resize for better responsiveness
+    const handleResize = () => {
+      s.resizeDrawingSurfaceToCanvas();
+    };
+    
+    window.addEventListener('resize', handleResize);
+    
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <div className="star">
+      <canvas id="starComponent" className="responsive-canvas"></canvas>
+    </div>
+  );
 }
