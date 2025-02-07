935bf8aaff5c34aa2dbcba70d1f06267f7ebce85
Nisarg Patel <23csd11@svitvasad.ac.in>
2025-02-07 14:05:33 +0530

    fix: Update event titles and navbar label for consistency

--- src/Components/EventCard.jsx
+++ src/Components/EventCard.jsx
@@ -126,7 +126,7 @@ const events = [
     id: 13,
     category: "Tech",
     title: "Civil Bridge Event",
-    details: "Not Given!!!",
+    details: "Cream sticks? For bridges? Get ready for Brawl Bridge!Teams race to build the most amazing structures, then watch the chaos unfold as they face the ultimate test of strength. Creativity vs. destruction – who will reign supreme? ",
   },
   {
     id: 14,
