56ecab761b688dafa8225704269db9075b8f8de5
Siddharth Panchal <1234siddharth.panchal@gmail.com>
2025-02-08 21:59:56 +0530

    Add @vercel/speed-insights dependency and integrate SpeedInsights component in App

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import Homepage from "./Components/Homepage"
 import EventCard from "./Components/EventCard"
 import AboutUs from "./Components/Aboutus"
 import Navbar from "./Components/Navbar"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 function App() {
   
 
   return (
     
     <BrowserRouter>
+    <SpeedInsights/>
     <Navbar/>
+
     <Routes>
       <Route path='/' element={<Homepage/>}/>
       <Route path='/events' element={<EventCard />}/>
