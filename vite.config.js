c334956e8184eb33eb61472ad3a4ec65c4628410
Siddharth Panchal <1234siddharth.panchal@gmail.com>
2025-02-10 00:11:10 +0530

    Add meta description and integrate sitemap plugin for improved SEO

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import viteCompression from "vite-plugin-compression2";
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteCompression from 'vite-plugin-compression2';
+import ViteSitemap from 'vite-plugin-sitemap'; // Import sitemap plugin
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(),
-        viteCompression({
-            algorithm: "brotliCompress", // Can be 'gzip', 'brotliCompress', or both
-            threshold: 1024, // Min file size to compress (in bytes)
-            verbose: true, // Show compression details in console
-          }),
-     ],
+  plugins: [
+    react(),
+    viteCompression({
+      algorithm: 'brotliCompress', // Can be 'gzip', 'brotliCompress', or both
+      threshold: 1024, // Min file size to compress (in bytes)
+      verbose: true, // Show compression details in console
+    }),
+    ViteSitemap({
+      hostname: 'https://prakarsh2025.vercel.app', // Replace with your website's hostname
+    }),
+  ],
 
-    assetsInclude: ['**/*.JPG', '**/*.HEIC']
-})
+  assetsInclude: ['**/*.JPG', '**/*.HEIC'],
+});
