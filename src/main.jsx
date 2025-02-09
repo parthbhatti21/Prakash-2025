8b0b2309d4b29f1590f3ca4a9edfa84cc4986828
Siddharth Panchal <1234siddharth.panchal@gmail.com>
2025-02-09 14:56:25 +0530

    Refactor EventRandom styles and implement lazy loading for App component with a loading screen

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,18 @@
+import React, { Suspense } from 'react';
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+
+const App = React.lazy(()=>import('./App.jsx'))
+const LoadingScreen = () => (
+  <div className="loading-screen">
+    <div className="loader"></div>
+    <p>Loading...</p>
+  </div>
+);
 
 // import './Components/style.css'
 createRoot(document.getElementById('root')).render(
-    <App />
+    <Suspense fallback={<LoadingScreen />}>
+        <App />
+      </Suspense>
   ,
 )
