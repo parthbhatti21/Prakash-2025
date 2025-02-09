781320e268aa5edd037e23fe9a82175c9f9331d1
yashm13114 <yashm13114@gmail.com>
2025-02-09 13:50:04 -0500

    added hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import { BrowserRouter, Route, Routes } from "react-router"
+import { HashRouter, Route, Routes } from "react-router"
 import Homepage from "./Components/Homepage"
 import EventCard from "./Components/EventCard"
 import AboutUs from "./Components/Aboutus"
 import Navbar from "./Components/Navbar"
 function App() {
-  
+
 
   return (
-    
-    <BrowserRouter>
-    <Navbar/>
 
-    <Routes>
-      <Route path='/' element={<Homepage/>}/>
-      <Route path='/events' element={<EventCard />}/>
-      {/* <Route path='/aboutus' element={<AboutPage/>}/> */}
-      <Route path='/aboutus' element={<AboutUs/>}/>
-    </Routes>
-    
-    </BrowserRouter>
+    <HashRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/events' element={<EventCard />} />
+        {/* <Route path='/aboutus' element={<AboutPage/>}/> */}
+        <Route path='/aboutus' element={<AboutUs />} />
+      </Routes>
+
+    </HashRouter>
   )
 }
 
