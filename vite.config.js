ebb52ec4dc44e1e33010c64e4503202f38f1a90a
[Parth <[parthbhatti021@gmail.com]>
2025-02-11 11:24:45 +0530

    Add sitemap generation plugins for improved SEO

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteCompression from "vite-plugin-compression2";
+import ViteSitemap from "vite-plugin-sitemap"
 
 
 // https://vite.dev/config/
@@ -11,6 +12,15 @@ export default defineConfig({
             threshold: 1024, // Min file size to compress (in bytes)
             verbose: true, // Show compression details in console
           }),
+          ViteSitemap({
+            hostname:'www.prakarsh.org',
+            routes:[
+                '/',
+                '/events',
+                '/aboutus'
+            ],
+          }),
+          
      ],
 
     assetsInclude: ['**/*.JPG', '**/*.HEIC']
